60ec547140ab562a623f8f10ac063ae082d502bc
yuki saito <ykst0622@gmail.com>
2022-06-28 15:07:52 +0900

    編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,29 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to new_task_path, notice: "タスクを追加しました！"
+    else 
+      render :new
+    end
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "ブログを編集しました"
+    else
+      render :new
+    end
   end
 
   private
