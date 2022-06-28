c0dab3685e78c767210855dbfd00d261b22c3839
yuki saito <ykst0622@gmail.com>
2022-06-28 14:32:52 +0900

    新規作成画面・詳細画面の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline, :status, :priority)
+  end
 end
