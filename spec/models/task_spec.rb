3d42eb193b382751447ef17cc5b3530ebfcfcd68
yuki saito <ykst0622@gmail.com>
2022-06-29 14:11:29 +0900

    テストデータ作成

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+require 'rails_helper'
+describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(title: '', content: '失敗テスト', deadline: '2022-07-06', status: '未着手', priority: '高')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: 'テスト', content: '', deadline: '2022-07-06', status: '未着手', priority: '高')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = Task.new(title: '成功テスト', content: '成功テスト', deadline: '2022-07-06', status: '未着手', priority: '高')
+        expect(task).to be_valid
+      end
+    end
+  end
+end
