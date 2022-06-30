c3d1c9b168dd35d6a7d9608e401a23e2b78f4cdd
yuki saito <ykst0622@gmail.com>
2022-06-30 12:55:09 +0900

    並び替えテストの実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,18 @@
 FactoryBot.define do
   factory :task do
-    title { 'test_title' }
-    content { 'test_content' }
-    deadline  { 'test_deadline' }
-    status  { 'test_status' }
-    priority  { 'test_priority' }
+    title { 'test_title1' }
+    content { 'test_content1' }
+    deadline  { 'test_deadline1' }
+    status  { 'test_status1' }
+    priority  { 'test_priority1' }
   end
+
+  factory :second_task, class: Task do
+    title { 'test_title2' }
+    content { 'test_content2' }
+    deadline  { 'test_deadline2' }
+    status  { 'test_status2' }
+    priority  { 'test_priority2' }
+  end
+
 end
