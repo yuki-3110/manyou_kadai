c8e71eb5533259ab4df79625b817311648479fed
yuki saito <ykst0622@gmail.com>
2022-07-05 11:09:53 +0900

    タイトルカラム 検索indexの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_015211) do
+ActiveRecord::Schema.define(version: 2022_07_05_020634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_07_04_015211) do
     t.integer "priority", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
