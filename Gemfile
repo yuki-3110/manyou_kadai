2b7e0fbf81803990864310769fd234411d49f23e
yuki saito <ykst0622@gmail.com>
2022-07-05 17:28:21 +0900

    ブートストラップで各viewを装飾

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'kaminari'
 
+gem 'bootstrap4-kaminari-views'
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,15 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
 
   def index
-    # @title = Task.group(:title).pluck(:title).sort
+    
     @tasks = Task.all.order(created_at: :desc).page(params[:page]).per(2)
 
-    # @tasks = Task.page(params[:page]).per(2)
-    # @tasks = Task.all.order(created_at: :desc) and return unless params[:task].present?
-    # @tasks = Task.where(status: params[:status])
-    @tasks = Task.all.order(deadline: :desc) if params[:sort_expired]
+    @tasks = Task.all.order(deadline: :desc).page(params[:page]).per(2) if params[:sort_expired]
 
-    @tasks = Task.all.order(priority: :asc) if params[:sort2_expired]
+    @tasks = Task.all.order(priority: :asc).page(params[:page]).per(2) if params[:sort2_expired]
 
     if params[:task].present?
       if params[:task][:status].present? && params[:task][:title].present?
@@ -23,30 +20,6 @@ class TasksController < ApplicationController
         @tasks = Task.search_with_title(params[:task][:title])
       end
     end
-
-    
-
-    # if params[:task].present?
-    #   if params[:task][:title].present?
-    #     @tasks = Task.search_with_title(params[:task][:title])
-    #   elsif params[:task][:status].present?
-    #     @tasks = Task.search_with_status(params[:task][:status])
-    #   end
-    # end
-
-    # if params[:task].present?
-    #   if params[:task][:title].present?
-    #     @tasks = Task.search_with_title(params[:task][:title])
-    #   end
-    # end
-
-    # @tasks = Task.search_with_title(params[:title]) if params[:title].present?
-    # @tasks = Task.search_with_status(params[:status]) if params[:status].present?
-
-    # @tasks = Task.search_with_title(params[:task][:title]) if params[:task][:title]
-    # @tasks = Task.search_with_status(params[:task][:status]) if params[:task]
-
-    # @tasks = @tasks.search_with_priority(params[:priority])
   end
 
   def new
@@ -86,11 +59,6 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     render :new if @task.invalid?
   end
-
-  # def search
-  #   @tasks = Task.where(priority: params[:priority])
-  #   # status_ids = Task.where("status = ?", params[:status]).pluck(:id)
-  # end
 
   private
 
