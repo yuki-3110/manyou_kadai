92e6d3d35de91f97845cc8ca4ee270a8b3413309
yuki saito <ykst0622@gmail.com>
2022-07-07 18:04:40 +0900

    シードデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+1.times do |n|
+  name = Faker::Avatar.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name, email: email, password: password,)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
