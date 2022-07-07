2118db17e3538a366aa2ff3056733a08e3ba5880
yuki saito <ykst0622@gmail.com>
2022-07-07 11:12:27 +0900

    セッションコントローラーを生成・ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#new'
+  resources :sessions, only: %i(new create destroy)
   resources :users, only: %i(new create show edit update)
   resources :tasks do
     collection do
